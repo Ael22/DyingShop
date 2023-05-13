08404b252ba7b29f2f6e33a3026d04aa59117cb7
Hoh Wei De <51860691+Ael22@users.noreply.github.com>
2023-05-13 19:14:57 +0800

    Create admin.js
    -create admin (not used on website yet)
    - login admin

---
+++ models/admin.js
@@ -0,0 +1,51 @@
+const pool = require("../database");
+const jwt = require("jsonwebtoken");
+
+const Admin = {
+  async createAdmin(username, password, callback) {
+    try {
+      const result = await pool.query(
+        `INSERT INTO admins (username, password) VALUES (?, ?)`,
+        [username, password]
+      );
+
+      console.log("Data insertion success!");
+      return callback(null, result);
+    } catch (err) {
+      console.error("Error executing the SQL Statement: ", err);
+      return callback(err, null);
+    }
+  },
+
+  async loginAdmin(username, password, callback) {
+    try {
+      const result = await pool.query(
+        `SELECT * FROM admins WHERE username = ? AND password = ?`,
+        [username, password]
+      );
+      if (result[0].length > 0) {
+        console.log("Admin Login Success!");
+        const token = jwt.sign(
+          {
+            id: result[0][0].id,
+            username: result[0][0].username,
+            adminAuth: true,
+          },
+          process.env.JWT_SECRET,
+          { expiresIn: 86400 }
+        );
+        return callback(null, token);
+      } else {
+        console.log("Invalid Username or Password");
+        return callback(null, null);
+      }
+    } catch (err) {
+      console.error("Error executing the SQL Statement: ", err);
+      return callback(err, null);
+    }
+  },
+
+  // TODO: Delete/Update Admin (ONLY IF product and category management is done and have time to do Admin/User Management)
+};
+
+module.exports = Admin;
