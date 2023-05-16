47c57cf022e6b0117ec101e25f8dd9e4b55bd713
Hoh Wei De <51860691+Ael22@users.noreply.github.com>
2023-05-16 19:36:42 +0800

    :sparkles: Link get/update product to frontend
    - :white_check_mark: New modal form for editing products
    - :white_check_mark: New Table to display products
    - :recycle: Reformated how index.js uses function in event listener

--- public/admin/dashboard/index.js
+++ public/admin/dashboard/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /**
  *  updates the admin dashboard links
  * @param {[HTMLElement]} element The element link button that needs to be update
@@ -23,6 +24,23 @@ function dashboardUiBtnUpdate(element) {
   }
 }
 
+function renderProductFormCategoryMenu(selectMenuId) {
+  fetch("/api/category")
+    .then((response) => response.json())
+    .then((data) => {
+      const { categories } = data;
+      const formSelectMenu = document.getElementById(selectMenuId);
+      for (let i = 0; i < categories.length; i += 1) {
+        const option = document.createElement("option");
+        option.text = categories[i].name;
+        option.value = categories[i].id;
+        formSelectMenu.add(option);
+      }
+    });
+}
+
+renderProductFormCategoryMenu("editProductCatgorySelectMenu");
+
 // Modal for createing new category
 const createCategoryModal = new bootstrap.Modal(
   document.getElementById("createCategoryModal")
@@ -43,10 +61,15 @@ const deleteModal = new bootstrap.Modal(
   document.getElementById("confirmDeleteModal")
 );
 
+const editProductModal = new bootstrap.Modal(
+  document.getElementById("editProductModal")
+);
+
 /**
  * Shows the modal that the admin needs to see
  * @param {*} modal The modal that you want to show
  */
+
 function showModal(modal) {
   modal.show();
 }
@@ -63,6 +86,17 @@ function hideModal(modal) {
  * loads/reloads the category page
  */
 function loadCategoryPage() {
+  document.getElementById("content-div").innerHTML = `
+  <div class="d-flex justify-content-center">
+  <div
+    class="spinner-border"
+    style="width: 3.6rem; height: 3.6rem"
+    role="status"
+  >
+    <span class="sr-only">Loading...</span>
+  </div>
+</div>
+  `;
   fetch("http://localhost:3000/api/category")
     .then((response) => response.json())
     .then((data) => {
@@ -144,6 +178,228 @@ function loadCategoryPage() {
     });
 }
 
+function loadCategoryCreateForm() {
+  const categoryData = {
+    name: document.getElementById("createCategoryNameInput").value,
+    description: document.getElementById("createCategoryDescInput").value,
+  };
+
+  fetch("/api/category", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(categoryData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      hideModal(createCategoryModal);
+      if (data.success_msg) {
+        document.getElementById("feedbackModalContent").innerText =
+          data.success_msg;
+      } else {
+        document.getElementById("feedbackModalContent").innerText =
+          data.err_msg;
+      }
+      showModal(feedbackModal);
+      loadCategoryPage();
+    });
+}
+
+function submitCategoryUpdateForm() {
+  const editData = {
+    id: document.getElementById("editCategoryIdInput").placeholder,
+    name: document.getElementById("editCategoryNameInput").value,
+    description: document.getElementById("editCategoryDescInput").value,
+  };
+
+  fetch("http://localhost:3000/api/category", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(editData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      hideModal(editModal);
+      if (data.success_msg) {
+        document.getElementById("feedbackModalContent").innerText =
+          data.success_msg;
+      } else {
+        document.getElementById("feedbackModalContent").innerText =
+          data.err_msg;
+      }
+      showModal(feedbackModal);
+      loadCategoryPage();
+    });
+}
+
+function loadConfirmDeleteCategoryAlert() {
+  const id = document.getElementById("editCategoryIdInput").placeholder;
+  fetch("/api/category", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id }),
+  })
+    .then((response) => {
+      hideModal(deleteModal);
+      if (response.ok) {
+        document.getElementById("feedbackModalContent").innerText =
+          "Deletion Success";
+        showModal(feedbackModal);
+      } else {
+        document.getElementById("feedbackModalContent").innerText =
+          "Deletion Failed";
+        showModal(feedbackModal);
+      }
+      loadCategoryPage();
+    })
+    .catch((err) => {
+      console.log("Error: ", err);
+    });
+}
+
+function loadProductPage() {
+  document.getElementById("content-div").innerHTML = `
+  <div class="d-flex justify-content-center">
+  <div
+    class="spinner-border"
+    style="width: 3.6rem; height: 3.6rem"
+    role="status"
+  >
+    <span class="sr-only">Loading...</span>
+  </div>
+</div>
+  `;
+  fetch("/api/product")
+    .then((response) => response.json())
+    .then((data) => {
+      let productContent = `
+  <h1 class="text-center text-bold"><strong>Product</strong></h1>
+        <button type="button" class="btn btn-primary" id="createProductBtn">
+          Create New Product
+        </button>
+        <table class="table table-striped table-bordered mt-3">
+          <thead>
+            <tr>
+              <th scope="col" class="col-1">ID</th>
+              <th scope="col" class="col-2">Name</th>
+              <th scope="col">Description</th>
+              <th scope="col" class="col-1">Price</th>
+              <th scope="col" class="col-1">Stock Quantity</th>
+              <th scope="col" class="col-1">Category Id</th>
+              <th scope="col" class="col-1 text-center">Edit Product</th>
+            </tr>
+          </thead>
+          <tbody class="table-group-divider">
+  `;
+      const { products } = data;
+      products.forEach((product) => {
+        const { id, name, description, price, stock_qty, category_id } =
+          product;
+        productContent += `
+        <tr>
+        <th scope="row">${id}</th>
+        <td>${name}</td>
+        <td>${description}</td>
+        <td>$${price}</td>
+        <td>${stock_qty}</td>
+        <td>${category_id}</td>
+        <td class="text-center">
+          <button type="button" class="btn btn-primary editProductBtn" id="editProductBtn${id}">
+            Edit
+          </button>
+        </td>
+      </tr>
+        `;
+      });
+      return productContent;
+    })
+    .then((table) => {
+      table += `
+      </tbody>
+      </table>
+      `;
+
+      const contentDiv = document.getElementById("content-div");
+      contentDiv.innerHTML = table;
+
+      const editProductButtons = document.getElementsByClassName(
+        "btn btn-primary editProductBtn"
+      );
+
+      for (let i = 0; i < editProductButtons.length; i += 1) {
+        editProductButtons[i].addEventListener("click", () => {
+          const productId = editProductButtons[i].id.replace(/[^\d.]/g, "");
+          fetch(`/api/product/${productId}`)
+            .then((response) => response.json())
+            .then((data) => {
+              const { product } = data;
+              document.getElementById("editProductIdInput").placeholder =
+                product.id;
+              document.getElementById("editProductNameInput").value =
+                product.name;
+              document.getElementById("editProductPriceInput").value =
+                product.price;
+              document.getElementById("editProductStockInput").value =
+                product.stock_qty;
+              document.getElementById("editProductDescInput").value =
+                product.description;
+              document.getElementById("editProductCatgorySelectMenu").value =
+                product.category_id;
+            });
+
+          showModal(editProductModal);
+        });
+      }
+    })
+    .catch((err) => {
+      console.error("Error: ", err);
+    });
+}
+
+function submitProductUpdateForm() {
+  const editData = {
+    id: parseInt(document.getElementById("editProductIdInput").placeholder, 10),
+    name: document.getElementById("editProductNameInput").value,
+    description: document.getElementById("editProductDescInput").value,
+    price: document.getElementById("editProductPriceInput").value,
+    stockQty: parseInt(
+      document.getElementById("editProductStockInput").value,
+      10
+    ),
+    categoryId: parseInt(
+      document.getElementById("editProductCatgorySelectMenu").value,
+      10
+    ),
+  };
+
+  fetch("/api/product", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(editData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      hideModal(editProductModal);
+      if (data.success_msg) {
+        document.getElementById("feedbackModalContent").innerText =
+          data.success_msg;
+      } else {
+        document.getElementById("feedbackModalContent").innerText =
+          data.err_msg;
+      }
+      showModal(feedbackModal);
+      loadProductPage();
+    });
+}
+
 // logout button event listener for user to logout properly
 document
   .getElementById("logout-btn")
@@ -191,70 +447,20 @@ document
     loadCategoryPage();
   });
 
+// event listener for when the admin dashboard's create category button is clicked
 document.getElementById("createCategoryBtn").addEventListener("click", () => {
-  const categoryData = {
-    name: document.getElementById("createCategoryNameInput").value,
-    description: document.getElementById("createCategoryDescInput").value,
-  };
-
-  fetch("/api/category", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(categoryData),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      hideModal(createCategoryModal);
-      if (data.success_msg) {
-        document.getElementById("feedbackModalContent").innerText =
-          data.success_msg;
-      } else {
-        document.getElementById("feedbackModalContent").innerText =
-          data.err_msg;
-      }
-      showModal(feedbackModal);
-      loadCategoryPage();
-    });
+  loadCategoryCreateForm();
 });
 
 // event listener for when the edit category modal's save changes button is clicked
 document.getElementById("updateCategoryBtn").addEventListener("click", () => {
-  const editData = {
-    id: document.getElementById("editCategoryIdInput").placeholder,
-    name: document.getElementById("editCategoryNameInput").value,
-    description: document.getElementById("editCategoryDescInput").value,
-  };
-
-  fetch("http://localhost:3000/api/category", {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(editData),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      hideModal(editModal);
-      if (data.success_msg) {
-        document.getElementById("feedbackModalContent").innerText =
-          data.success_msg;
-      } else {
-        document.getElementById("feedbackModalContent").innerText =
-          data.err_msg;
-      }
-      showModal(feedbackModal);
-      loadCategoryPage();
-    });
+  submitCategoryUpdateForm();
 });
 
 // event listener for when the edit modal's delete button is clicked
 document.getElementById("deleteCategoryBtn").addEventListener("click", () => {
   hideModal(editModal);
   const id = document.getElementById("editCategoryIdInput").placeholder;
-  // TODO: need do check if it is category or product etc.
   document.getElementById(
     "confirmDeleteModalContent"
   ).innerHTML = `You are <strong>DELETING category</strong> with <strong>ID ${id}</strong>`;
@@ -265,30 +471,7 @@ document.getElementById("deleteCategoryBtn").addEventListener("click", () => {
 document
   .getElementById("confirmConfirmDeleteBtn")
   .addEventListener("click", () => {
-    const id = document.getElementById("editCategoryIdInput").placeholder;
-    fetch("/api/category", {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ id }),
-    })
-      .then((response) => {
-        hideModal(deleteModal);
-        if (response.ok) {
-          document.getElementById("feedbackModalContent").innerText =
-            "Deletion Success";
-          showModal(feedbackModal);
-        } else {
-          document.getElementById("feedbackModalContent").innerText =
-            "Deletion Failed";
-          showModal(feedbackModal);
-        }
-        loadCategoryPage();
-      })
-      .catch((err) => {
-        console.log("Error: ", err);
-      });
+    loadConfirmDeleteCategoryAlert();
   });
 
 // event listener for when the admin dashboard's product button is clicked
@@ -298,11 +481,9 @@ document
     event.preventDefault();
 
     dashboardUiBtnUpdate(document.getElementById("product-btn"));
-
-    const contentDiv = document.getElementById("content-div");
-    contentDiv.innerHTML = `
-      <strong>
-        Product
-      </strong>
-    `;
+    loadProductPage();
   });
+
+document.getElementById("updateProductBtn").addEventListener("click", () => {
+  submitProductUpdateForm();
+});
