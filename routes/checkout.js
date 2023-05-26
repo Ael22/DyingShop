2712023a5ccc2b7f2ea13afbc0c85bd315a47d96
weide <HWEIDE.21@ichat.sp.edu.sg>
2023-05-26 11:20:02 +0800

    :construction: Initializing new routes
    - New checkout route added (POST)
    - No testing done yet

---
+++ routes/checkout.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+const router = express.Router();
+
+const DOMAIN = "http://localhost:3000";
+
+router.post("/checkout", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    Line_items: [
+      {
+        price_data: {
+          currency: "sgd",
+          product_data: {
+            name: "T-shirt",
+          },
+          unit_amount: 2200,
+          tax_behavior: "exclusive",
+        },
+        quantity: 5,
+      },
+    ],
+    mode: "payment",
+    success_url: `${DOMAIN}/success.html`,
+    cancel_url: `${DOMAIN}/cancel.html`,
+  });
+
+  res.redirect(303, session.url);
+});
+
+module.exports = router;
