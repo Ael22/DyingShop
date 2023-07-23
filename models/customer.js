479eead78740968ace12aa94c056b37b2b75d54d
Hoh Wei De <51860691+Ael22@users.noreply.github.com>
2023-07-24 01:36:02 +0800

    :sparkles: Customer verification model methods
    - New customer model methods for checking, user requesting and updating verification on the database

--- models/customer.js
+++ models/customer.js
@@ -21,7 +21,7 @@ const customer = {
   async getCustomerById(id) {
     try {
       const result = await pool.query(
-        `SELECT first_name, last_name, email, created_at FROM customers WHERE id = ?`,
+        `SELECT first_name, last_name, email, created_at, verified FROM customers WHERE id = ?`,
         [id]
       );
       return result[0][0];
@@ -131,6 +131,69 @@ const customer = {
       ]);
       console.log("Query Executed");
       // Checks if any row got deleted
+      if (result[0].affectedRows < 1) {
+        // return false if no rows got deleted
+        return false;
+      }
+      // Passed all checks so return true
+      return true;
+    } catch (err) {
+      // An error got caught log it
+      console.error("Error executing the SQL Statement: ", err);
+      // Throw error to be caught
+      throw err;
+    }
+  },
+
+  async checkEmailVerification(id) {
+    try {
+      const result = await pool.query(
+        "SELECT verified FROM customers WHERE id = ?",
+        [id]
+      );
+
+      return result;
+    } catch (err) {
+      // An error got caught log it
+      console.error("Error executing the SQL Statement: ", err);
+      // Throw error to be caught
+      throw err;
+    }
+  },
+
+  async updateVerifyToken(id) {
+    try {
+      const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: 86400,
+      });
+
+      // sends a query to the database
+      const result = await pool.query(
+        `UPDATE customers SET verify_token = ? WHERE id = ?`,
+        [token, id]
+      );
+      console.log("Query Executed");
+      // Checks if any row got deleted
+      if (result.affectedRows < 1) {
+        // return false if no rows got deleted
+        return false;
+      }
+      // Passed all checks so return true
+      return token;
+    } catch (err) {
+      // An error got caught log it
+      console.error("Error executing the SQL Statement: ", err);
+      // Throw error to be caught
+      throw err;
+    }
+  },
+
+  async verifyEmail(token, id) {
+    try {
+      const result = await pool.query(
+        `UPDATE customers SET verified = ?, verify_token = ? WHERE verify_token = ? AND id = ? `,
+        [1, null, token, id]
+      );
       if (result[0].affectedRows < 1) {
         // return false if no rows got deleted
         return false;
